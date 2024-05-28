89b30fbad92ee86ed2ff65e380b1381dcd02eeed
dhyey <dhyeyrabadiya2030@gmaol.com>
2024-05-28 10:24:49 +0530

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:api_testing/post.dart';
 import 'package:flutter/material.dart';
 
 import 'home_screen.dart';
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:FatchData1()
+      home:MyHomePage()
     );
   }
 }
---
+++ lib/post.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+// void main() {
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter POST API Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: MyHomePage(),
+//     );
+//   }
+// }
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _formKey = GlobalKey<FormState>();
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  bool _isLoading = false;
+  String? _response;
+
+  Future<void> _submitData() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    final String apiUrl = 'https://jsonplaceholder.typicode.com/posts';
+    final response = await http.post(
+      Uri.parse(apiUrl),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'name': _nameController.text,
+        'email': _emailController.text,
+      }),
+    );
+
+    setState(() {
+      _isLoading = false;
+      _response = response.body;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter POST API Demo'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                controller: _nameController,
+                decoration: InputDecoration(labelText: 'Name'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your name';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(labelText: 'Email'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              _isLoading
+                  ? CircularProgressIndicator()
+                  : ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _submitData();
+                  }
+                },
+                child: Text('Submit'),
+              ),
+              SizedBox(height: 20),
+              _response != null ? Text('Response: $_response') : Container(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
